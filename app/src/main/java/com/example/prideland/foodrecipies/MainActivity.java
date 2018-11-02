b499a737e4d4a888d2455644fa907a98cff23130
ikuaj <ikuaj96@gmail.com>
2018-11-02 17:43:38 +0300

    New font

--- app/src/main/java/com/example/prideland/foodrecipies/MainActivity.java
+++ app/src/main/java/com/example/prideland/foodrecipies/MainActivity.java
@@ -1,23 +1,27 @@
 package com.example.prideland.foodrecipies;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button mfindFoodButton;
     private EditText mfoodEditText;
+    private TextView mtitleTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mtitleTextView = (TextView) findViewById(R.id.titleTextView);
+        Typeface mali = Typeface.createFromAsset(getAssets(), "fonts/GrandHotel-Regular.otf");
+        mtitleTextView.setTypeface(mali);
         mfoodEditText = (EditText) findViewById(R.id.foodEditText);
         mfindFoodButton = (Button) findViewById(R.id.findFoodbutton);
         mfindFoodButton.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/prideland/foodrecipies/SearchActivity.java
+++ app/src/main/java/com/example/prideland/foodrecipies/SearchActivity.java
@@ -3,17 +3,41 @@ package com.example.prideland.foodrecipies;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SearchActivity extends AppCompatActivity {
     private TextView mfoodTextView;
+    private ListView mListView;
+    private String[] food = new String[] {"Mi Mero Mole", "Mother's Bistro",
+                "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+                "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
+                "Lardo", "Portland City Grill", "Fat Head's Brewery",
+                "Chipotle", "Subway"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        mListView = (ListView) findViewById(R.id.listView);
         mfoodTextView = (TextView) findViewById(R.id.foodTextView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, food);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String restaurant = ((TextView)view).getText().toString();
+                Toast.makeText(SearchActivity.this, restaurant, Toast.LENGTH_LONG).show();
+            }
+        });
+
         Intent intent = getIntent();
         String food = intent.getStringExtra("food");
         mfoodTextView.setText("Here is the recipie of your: " + food);
